47a5f42b267ee1d1e0af1c815c03845264386a16
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-03-02 22:35:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import config
 import telebot
 import datetime
+import bs4
+import requests
 from telebot import types
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(commands=["start", "leto"])
+@bot.message_handler(commands=["start"])
 def get_start(message):
 
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button_summer = types.KeyboardButton(text="Сколько дней до лета?")
+    button_today = types.KeyboardButton(text = "Скажи погоду на сегодня")
+    button_tor = types.KeyboardButton(text = "Скажи погоду на завтра")
     button_help = types.KeyboardButton(text="/help")
-    keyboard.add(button_summer, button_help)
-    bot.send_message(message.chat.id,
-                     "Помогу тебе посчитать, сколько дней осталось до лета",
+    keyboard.add(button_summer, button_today,button_tor, button_help)
+    bot.send_message(message.chat.id,"Я забагованная хреновина, не ругайся, может что и смогу",
                      reply_markup=keyboard)
 
 
@@ -29,17 +32,30 @@ def get_summer(message):
                          + " уважаемый " + message.chat.first_name)
 
 
+@bot.message_handler(func = lambda msg: msg.text == 'Скажи погоду на сегодня')
+def get_today(message):
+    s = requests.get('https://www.meteoservice.ru/weather/text/moskva')
+    b = bs4.BeautifulSoup(s.text, "html.parser")
+    p3 = b.select('.words p')
+    bot.send_message(message.chat.id, p3[0].getText())
+
+
+@bot.message_handler(func = lambda msg: msg.text == 'Скажи погоду на завтра')
+def get_tor(message):
+    s = requests.get('https://www.meteoservice.ru/weather/text/moskva')
+    b = bs4.BeautifulSoup(s.text, "html.parser")
+    p3 = b.select('.words p')
+    bot.send_message(message.chat.id, p3[1].getText())
+
+
+
 @bot.message_handler(commands=["help"])
-def get_help(message):
+def get_summer(message):
     bot.send_message(message.chat.id,
                      "привет, холодный кусок мяса, "
                      "я подарю тебе надежду в этом ледяном аду, "
                      "я подарю тебе веру в то, что рано или поздно придёт тепло")
 
-@bot.message_handler(content_types=["text"])
-def get_text(message):
-    bot.send_message(message.chat.id,
-                     "Просто введи /leto и я помогу тебе узнать дни до него")
-
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+1
